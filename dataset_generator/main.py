f133ab189fbfc899db881b97a308136df6779b07
Merkushev Egor <nounstep@gmail.com>
2023-09-08 23:08:04 +0300

    Improved GUI

--- dataset_generator/main.py
+++ dataset_generator/main.py
@@ -1,13 +1,17 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
+# import tkinter as tk
+# from tkinter import ttk
+# from tkinter import filedialog
 from datetime import datetime, timedelta
 import random
 import string
 import time
 import pandas as pd
 import io
-from ttkthemes import ThemedStyle
+import customtkinter as ctk
+from customtkinter import filedialog
+
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue") # Themes: "blue", "green", "dark-blue"
 
 def generate_alphanum_random_string(length) -> str:
     letters_and_digits = string.ascii_lowercase + string.digits
@@ -21,17 +25,27 @@ def get_season(date) -> str:
     else: return 'Winter'
 
 def generate_data():
+    status_label.configure(text=f"В процессе...", text_color="yellow")
     try:
-        status_label.config(text=f"In progress...")
-        N = int(N_entry.get())
+        N = N_entry.get()
+        if N != "": N = int(N)
+        else: N = 100
+
         str_begin_date = begin_date_entry.get()
-        begin_date = datetime.strptime(str_begin_date, '%d.%m.%Y')
+        if str_begin_date != "": begin_date = datetime.strptime(str_begin_date, '%d.%m.%Y')
+        else:
+            str_begin_date = "01.01.2000"
+            begin_date = datetime.strptime(str_begin_date, '%d.%m.%Y')
 
         str_end_date = end_date_entry.get()
-        end_date = datetime.strptime(str_end_date, '%d.%m.%Y')
+        if str_end_date != "": end_date = datetime.strptime(str_end_date, '%d.%m.%Y')
+        else:
+            str_end_date = "31.12.2020"
+            end_date = datetime.strptime(str_end_date, '%d.%m.%Y')
 
-        flag = int(multiplier_var.get())
-        if (flag == 0): multiplier = int(multiplier_entry.get())
+        multiplier = multiplier_entry.get()
+        if multiplier != "": multiplier = int(multiplier)
+        else: multiplier = random.randint(20, 360)
 
         # Getting platforms dictionary
         platforms = open("platforms.txt").readlines()
@@ -75,10 +89,7 @@ def generate_data():
         # Generating time of view
         time_of_view = [None] * N
         for i in range(len(time_of_view)):
-            if (flag == 1):
-                time_of_view[i] = time.strftime("%H:%M:%S", time.gmtime(number_of_ads[i] * random.randint(20, 360)))
-            else:
-                time_of_view[i] = time.strftime("%H:%M:%S", time.gmtime(number_of_ads[i] * multiplier))
+            time_of_view[i] = time.strftime("%H:%M:%S", time.gmtime(number_of_ads[i] * multiplier))
 
         # Getting the types of ad
         types_of_ad = {}
@@ -136,56 +147,49 @@ def generate_data():
                 writer.sheets['ads_database'].set_column(col_idx + 1, col_idx + 1, column_width)
 
             writer.save()
-            status_label.config(text=f"Data saved to {save_path}")
+            status_label.configure(text=f"Файл сохранен в {save_path}", text_color="green")
+        else:
+            status_label.configure(text=f"Операция отменена", text_color="grey")
     except:
-        status_label.config(text=f"Error")
+        status_label.configure(text=f"Ошибка", text_color="red")
+
 
 def end_func():
     exit(0)
 
 if __name__ == '__main__':
     # Create the main window
-    window = tk.Tk()
+    window = ctk.CTk()
     window.title("Ad Data Generator")
 
-    # # Apply a Material Design theme to the window
-    # style = ThemedStyle(window)
-    # style.set_theme("plastik")
-
     # Create and place GUI components
-    N_label = ttk.Label(window, text="Количество строк:")
+    N_label = ctk.CTkLabel(window, text="Количество строк:", )
     N_label.grid(column=0, row=0, padx=10, pady=5)
-    N_entry = ttk.Entry(window)
+    N_entry = ctk.CTkEntry(window, placeholder_text="100")
     N_entry.grid(column=1, row=0, padx=10, pady=5)
 
-    begin_date_label = ttk.Label(window, text="Дата начала (дд.мм.гггг):")
+    begin_date_label = ctk.CTkLabel(window, text="Дата начала (дд.мм.гггг):")
     begin_date_label.grid(column=0, row=1, padx=10, pady=5)
-    begin_date_entry = ttk.Entry(window)
+    begin_date_entry = ctk.CTkEntry(window, placeholder_text="01.01.2000")
     begin_date_entry.grid(column=1, row=1, padx=10, pady=5)
 
-    end_date_label = ttk.Label(window, text="Дата конца (дд.мм.гггг):")
+    end_date_label = ctk.CTkLabel(window, text="Дата конца (дд.мм.гггг):")
     end_date_label.grid(column=0, row=2, padx=10, pady=5)
-    end_date_entry = ttk.Entry(window)
+    end_date_entry = ctk.CTkEntry(window, placeholder_text="31.12.2020")
     end_date_entry.grid(column=1, row=2, padx=10, pady=5)
 
-    multiplier_label = ttk.Label(window, text="Генерировать время просмотра автоматически?")
-    multiplier_label.grid(column=0, row=3, padx=10, pady=5)
-    multiplier_var = tk.IntVar()
-    multiplier_checkbutton = ttk.Checkbutton(window, text="Да", variable=multiplier_var)
-    multiplier_checkbutton.grid(column=1, row=3, padx=10, pady=5)
-
-    multiplier_entry_label = ttk.Label(window, text="Коэффицент для времени просмотра:")
+    multiplier_entry_label = ctk.CTkLabel(window, text="Коэффицент для времени просмотра:")
     multiplier_entry_label.grid(column=0, row=4, padx=10, pady=5)
-    multiplier_entry = ttk.Entry(window)
+    multiplier_entry = ctk.CTkEntry(window, placeholder_text="Автоматически")
     multiplier_entry.grid(column=1, row=4, padx=10, pady=5)
 
-    generate_button = ttk.Button(window, text="Сгенерировать", command=generate_data)
+    generate_button = ctk.CTkButton(window, text="Сгенерировать", command=generate_data)
     generate_button.grid(column=0, row=5, columnspan=2, pady=5)
 
-    exit_button = ttk.Button(window, text="Выход", command=end_func)
+    exit_button = ctk.CTkButton(window, text="Выход", command=end_func)
     exit_button.grid(column=0, row=6, columnspan=3, pady=5)
 
-    status_label = ttk.Label(window, text="")
+    status_label = ctk.CTkLabel(window, text="")
     status_label.grid(column=0, row=7, columnspan=2, pady=5)
 
     # Start the GUI event loop
